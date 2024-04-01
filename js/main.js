775acfc7e8836510c293390b8de6ac9eee69b8a2
yanxun2474 <yanxun2474@gmail.com>
2024-04-01 20:25:41 +0800

    標籤篩選切換樣式

--- js/main.js
+++ js/main.js
@@ -15,6 +15,13 @@ let data = [
 ];
 
 
+function init(){
+    renderList(data);
+    addItem(data);
+    delItem(data);
+}
+init();
+
 //渲染列表
 function renderList(data){
     let tmpStr = "";
@@ -29,10 +36,10 @@ function renderList(data){
     });
     list.innerHTML = tmpStr;
 }
-renderList(data);
 
 //新增事項功能
-addBtn.addEventListener("click",(e)=>{
+function addItem(data){
+    addBtn.addEventListener("click",(e)=>{
     if(input.value.trim() === ""){
         alert("請輸入文字!!!");
         return;
@@ -45,19 +52,33 @@ addBtn.addEventListener("click",(e)=>{
     renderList(data);
     input.value = "";
 });
+}
 
 //刪除事項
-list.addEventListener("click",e=>{
-    
+function delItem(data){
+    list.addEventListener("click",e=>{
     if(e.target.getAttribute("class") !== "delete"){
-        alert("1");
         return;
     }
     let index = e.target.getAttribute("data-index");
-    console.log(index);
     data.splice(index,1);
     renderList(data);
-})
+});
+}
 
 //篩選事項
 
+//切換篩選標籤
+const filter = document.querySelector(".filter");
+const filter_Li = document.querySelectorAll(".filter li");
+console.log(filter);
+console.log(filter_Li);
+filter.addEventListener("click",e=>{
+    if(e.target.nodeName !== "LI"){
+        return;
+    }
+    filter_Li.forEach((item)=>{
+        item.setAttribute("class","");
+    });
+    e.target.setAttribute("class","active");
+});
